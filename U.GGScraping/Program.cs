1863e4c9320603dbc13e866e3a9493a3521594a7
Alex Arabadjiev <alex.arabadjiev05@gmail.com>
2020-12-02 16:07:38 +0200

    Collect win/play/ban rate and total matches

--- U.GGScraping/Program.cs
+++ U.GGScraping/Program.cs
@@ -16,37 +16,63 @@ namespace U.GGScraping
 
             var document = await context.OpenAsync("https://u.gg/lol/champions/aatrox/build");
 
-            string selector = string.Empty;
-            IElement element;
+            //Test
+
+            //var elements = document.QuerySelector(SelectorConstants.ChampionRankingStatsSection);
+            //Console.WriteLine(elements.TextContent);
 
             //Lane
 
-            selector = SelectorConstants.ChampionHeader;
-            element = document.QuerySelector(selector);
+            string championHeaderSelector = SelectorConstants.ChampionHeader;
+            var championHeaderElement = document.QuerySelector(championHeaderSelector);
 
             string lanePattern = "(?<=for )[A-Z]{1}[a-z]+";
 
-            string lane = Regex.Match(element.TextContent, lanePattern).ToString();
+            string lane = Regex.Match(championHeaderElement.TextContent, lanePattern).ToString();
 
             //Tier
 
-            char tier = element.TextContent[0];
+            char tier = championHeaderElement.TextContent[0];
 
             //Patch
 
             string patchPattern = "[0-9]+.[0-9]+";
 
-            string patch = Regex.Match(element.TextContent, patchPattern).ToString();
+            string patch = Regex.Match(championHeaderElement.TextContent, patchPattern).ToString();
 
             //WinRate
 
-            
+            var championRankingStatsElement = document.QuerySelector(SelectorConstants.ChampionRankingStatsSection);
+
+            string winRatePattern = "[0-9.]+(?=%Win)";
+
+            string winRate = Regex.Match(championRankingStatsElement.TextContent, winRatePattern).ToString();
+
+            Console.WriteLine(winRate);
 
             //PickRate
 
+            string pickRatePattern = "[0-9.]+(?=%Pick)";
+
+            string pickRate = Regex.Match(championRankingStatsElement.TextContent, pickRatePattern).ToString();
+
+            Console.WriteLine(pickRate);
 
             //BanRate
 
+            string banRatePattern = "[0-9.]+(?=%Ban)";
+                   
+            string banRate = Regex.Match(championRankingStatsElement.TextContent, banRatePattern).ToString();
+
+            Console.WriteLine(banRate);
+
+            //Matches
+
+            string totalMatchesPattern = "[0-9,]+(?=Matches)";
+
+            string totalMatches = Regex.Match(championRankingStatsElement.TextContent, totalMatchesPattern).ToString();
+
+            Console.WriteLine(totalMatches);
 
             //SummonerSpells
 
