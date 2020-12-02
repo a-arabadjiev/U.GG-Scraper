98cd14a880ea735085af7b1c44f129e133e6d934
Alex Arabadjiev <alex.arabadjiev05@gmail.com>
2020-12-02 15:29:29 +0200

    Gather all selectors needed; Collect lane, tier and patch values

---
+++ U.GGScraping/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+
+namespace U.GGScraping
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+
+            var document = await context.OpenAsync("https://u.gg/lol/champions/aatrox/build");
+
+            string selector = string.Empty;
+            IElement element;
+
+            //Lane
+
+            selector = SelectorConstants.ChampionHeader;
+            element = document.QuerySelector(selector);
+
+            string lanePattern = "(?<=for )[A-Z]{1}[a-z]+";
+
+            string lane = Regex.Match(element.TextContent, lanePattern).ToString();
+
+            //Tier
+
+            char tier = element.TextContent[0];
+
+            //Patch
+
+            string patchPattern = "[0-9]+.[0-9]+";
+
+            string patch = Regex.Match(element.TextContent, patchPattern).ToString();
+
+            //WinRate
+
+            
+
+            //PickRate
+
+
+            //BanRate
+
+
+            //SummonerSpells
+
+
+            //MainRuneTreeName
+
+
+            //MainRuneTreeRunes
+
+
+            //SecondaryRuneTreeName
+
+
+            //SecondaryRuneTreeRunes
+
+
+            //RunesWinRate
+
+
+            //Offense
+
+
+            //Flex
+
+
+            //Defense
+
+
+            //SkillPriority
+
+
+            //StartingItems
+
+
+            //MythicAndCoreItems
+
+
+            //FourthItemOptions
+
+
+            //FifthItemOptions
+
+
+            //SixthItemOptions
+
+
+        }
+    }
+}
---
+++ U.GGScraping/SelectorConstants.cs
@@ -0,0 +1,25 @@
+﻿namespace U.GGScraping
+{
+    public class SelectorConstants
+    {
+        public const string ChampionHeader = "div.champion-header";
+
+        public const string ChampionRankingStatsSection = "div.champion-profile-page > div > div.content-section.champion-ranking-stats";
+
+        public const string SummonerSpellsSection = "div.champion-profile-page > div > div.content-section.content-section_no-padding.grid-1 > div.content-section_content.summoner-spells";
+
+        public const string RunesSection = "div.champion-profile-page > div > div.content-section.content-section_no-padding.grid-1";
+
+        public const string CounterChampionsSection = "div.champion-profile-page > div > div.content-section.toughest-matchups.undefined > div.matchups";
+
+        public const string SkillsSection = "div.champion-profile-page > div > div.content-section.content-section_no-padding.recommended-build_skills";
+
+        public const string StartingAndCoreItemsSection = "div.champion-profile-page > div > div.content-section.content-section_no-padding.recommended-build_items.media-query.media-query_TABLET__DESKTOP_SMALL > div.content-section_content.starting-items";
+
+        public const string FourthItemSection = "div.champion-profile-page > div > div.content-section.content-section_no-padding.recommended-build_items.media-query.media-query_TABLET__DESKTOP_SMALL > div.content-section_content.item-options.item-options-1";
+
+        public const string FifthItemSection = "div.champion-profile-page > div > div.content-section.content-section_no-padding.recommended-build_items.media-query.media-query_TABLET__DESKTOP_SMALL > div.content-section_content.item-options.item-options-2";
+
+        public const string SixthItemSection = "div.champion-profile-page > div > div.content-section.content-section_no-padding.recommended-build_items.media-query.media-query_TABLET__DESKTOP_SMALL > div.content-section_content.item-options.item-options-3";
+    }
+}
